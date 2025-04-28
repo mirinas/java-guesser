7a5e9dea30469a2cc751f7b97fb13cdc712c75ef
mirinas <augustas.mirinas@gmail.com>
2025-04-28 19:55:18 +0300

    exercise completed

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,35 @@ public class Main {
         int atsakymas = (int) (Math.random() * 100) + 1;
         Scanner s = new Scanner(System.in);
 
+        System.out.println("Hello! Welcome to a guessing game.\nGuess the number and you win :))\nHow many tries do you need?");
+        int limitas = s.nextInt();
+
+        System.out.println("Enter a number 1-100:");
+
+        int spejimuKiekis = 0;
+        while(spejimuKiekis < limitas) {
+
+            System.out.println("You have " + (limitas - spejimuKiekis) + " attempts left");
+
+            int spejimas = s.nextInt();
+            spejimuKiekis++;
+//            spejimuKiekis += 1;
+//            spejimuKiekis = spejimuKiekis + 1;
+
+            if(spejimas > atsakymas) {
+                System.out.println("Guess less");
+            } else if(spejimas < atsakymas) {
+                System.out.println("Guess more");
+            } else {
+                System.out.println("Correct! You win");
+                return;
+            }
+
+        }
+
+        System.out.println("You lose!");
+
+
         // 1. perskaitom naudotojo spejima
         // 2. jeigu spejimas didesnis nei turi buti
         //      rasom "spek mazesni skaiciu"
