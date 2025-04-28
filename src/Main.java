ca03c4bd2179d00faa981001bdd0bc80e90d2020
mirinas <augustas.mirinas@gmail.com>
2025-04-28 18:09:23 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 1 - 100
+        int atsakymas = (int) (Math.random() * 100) + 1;
+        Scanner s = new Scanner(System.in);
+
+        // 1. perskaitom naudotojo spejima
+        // 2. jeigu spejimas didesnis nei turi buti
+        //      rasom "spek mazesni skaiciu"
+        //      else rasom "spek didesni skaiciu"
+        //         griztam i pirma zingsni
+
+        // 3. jei nei didesnis, nei mazesnis - naudotojas pataike!
+        // 4. parasom sekmes zinute
+
+
+
+        // BONUS:
+        // 1. isijungus programa, naudotojas lazinasi is kiek kartu
+        //      atspes skaiciu
+        // 2. jeigu naudotojas virsija spejimu kieki, uzdarom programa
+        // 3. rasom pralaimejai
+        // 4. pries kiekviena spejima rasom kiek liko bandymu
+    }
+}
